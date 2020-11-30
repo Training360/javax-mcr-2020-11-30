6f018596d5d6c177fbd4861819e3edd4aebac14d
iviczian <viczian.istvan@gmail.com>
2020-11-30 13:26:45 +0100

    Java config for Service

---
+++ src/main/java/training/employees/ServiceConfig.java
@@ -0,0 +1,13 @@
+package training.employees;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+//    @Bean
+//	public HelloService helloService() {
+//		return new HelloService();
+//	}
+}
