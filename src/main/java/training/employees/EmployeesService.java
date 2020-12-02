f410e3397fa942825f52ff267f5d0ea4de3cb5ad
iviczian <viczian.istvan@gmail.com>
2020-12-02 10:05:26 +0100

    Repostory

--- src/main/java/training/employees/EmployeesService.java
+++ src/main/java/training/employees/EmployeesService.java
@@ -4,68 +4,51 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 @Slf4j
 public class EmployeesService {
 
-    private static final AtomicLong idGenerator = new AtomicLong();
-
-    private static final List<Employee> employees =
-            new ArrayList<>(List.of(new Employee(idGenerator.incrementAndGet(), "John Doe"),
-                    new Employee(idGenerator.incrementAndGet(), "Jane Doe")));
+    private final EmployeesRepository employeesRepository;
 
     private final ModelMapper modelMapper;
 
     public List<EmployeeDto> findAll(Optional<String> prefix) {
         java.lang.reflect.Type targetListType = new TypeToken<List<EmployeeDto>>() {}.getType();
-        return modelMapper.map(employees.stream()
-                .filter(e -> prefix.isEmpty() || e.getName().startsWith(prefix.get()))
-                .collect(Collectors.toList())
+        return modelMapper.map(employeesRepository.findAll()
                 , targetListType);
     }
 
     public EmployeeDto findById(long id) {
-        return employees.stream()
-                .filter(e -> e.getId() == id)
-                .map(e -> modelMapper.map(e, EmployeeDto.class))
-                .findAny().orElseThrow(() -> new EmployeeNotFoundException(id));
+        return modelMapper.map(employeesRepository.findById(id), EmployeeDto.class);
     }
 
     public EmployeeDto create(CreateEmployeeCommand command) {
         log.info("Create employee");
         log.debug("Create employee with name" + command.getName());
-        Employee employee = new Employee(idGenerator.incrementAndGet(), command.getName());
-        employees.add(employee);
+        Employee employee = new Employee(command.getName());
+        employeesRepository.create(employee);
         return modelMapper.map(employee, EmployeeDto.class);
     }
 
     public EmployeeDto update(long id, UpdateEmployeeCommand command) {
-        Employee employee = employees.stream()
-                .filter(e -> e.getId() == id)
-                .findAny().orElseThrow(() -> new EmployeeNotFoundException(id));
-        employee.setName(command.getName());
+        employeesRepository.update(id, command.getName());
+        Employee employee = new Employee(id, command.getName());
         return modelMapper.map(employee, EmployeeDto.class);
     }
 
     public void delete(long id) {
-        Employee employee = employees.stream()
-                .filter(e -> e.getId() == id)
-                .findAny().orElseThrow(() -> new EmployeeNotFoundException(id));
-        employees.remove(employee);
+        employeesRepository.delete(id);
     }
 
     public void clear() {
-        employees.clear();
+
+        // TODO Teszthez kell?
+
     }
 }
